74dfa1ef5de3aae7d8b635da1d5332ebbace08c6
Vincenzo Morisco <vincenzomorisco@hotmail.it>
2024-02-12 18:41:32 +0100

    Fix paths

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,15 @@
+const currentPathname = window.location.pathname;
+let path = currentPathname === '/Portfolio/index.html' ? './pages/dynamic-page.html?' : '../pages/dynamic-page.html?'
+
 var linksHtml = `
         <ul>
-            <li><a href="../pages/dynamic-page.html?content=cat1" class="dynamic-load">2010 - 2015</a></li>
-            <li><a href="../pages/dynamic-page.html?content=cat2" class="dynamic-load">2016 - 2020</a></li>
-            <li><a href="../pages/dynamic-page.html?content=cat3" class="dynamic-load">2021 - now</a></li>
+            <li><a href="${path}content=cat1" class="dynamic-load">2010 - 2015</a></li>
+            <li><a href="${path}content=cat2" class="dynamic-load">2016 - 2020</a></li>
+            <li><a href="${path}content=cat3" class="dynamic-load">2021 - now</a></li>
             <br>
-            <li><a href="../pages/dynamic-page.html?content=about" class="dynamic-load">about</a></li>
-            <li><a href="../pages/dynamic-page.html?content=exhibitions" class="dynamic-load">exhibitions</a></li>
-            <li><a href="../pages/dynamic-page.html?content=contact" class="dynamic-load">contact</a></li>
+            <li><a href="${path}content=about" class="dynamic-load">about</a></li>
+            <li><a href="${path}content=exhibitions" class="dynamic-load">exhibitions</a></li>
+            <li><a href="${path}content=contact" class="dynamic-load">contact</a></li>
         </ul>
     `;
 var menuContainer = document.getElementById('menu-container');
