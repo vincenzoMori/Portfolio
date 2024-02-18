d0413df391d030720355d01096727269dfd7b08a
Antonello <anto.dipas@gmail.com>
2024-02-18 23:16:55 +0100

    refactor dynamic pages

--- assets/js/image-slider.js
+++ assets/js/image-slider.js
@@ -1,3 +1,4 @@
+
 let currentImageIdx = 0;
 let images = [];
 let imageOpened = false;
@@ -6,101 +7,17 @@ let prevBlobUrl = '';
 let currBlobUrl = '';
 let nextBlobUrl = '';
 
-document.addEventListener('DOMContentLoaded', function () {
-    function getQueryParam(param) {
-        const queryParams = new URLSearchParams(window.location.search);
-        return queryParams.get(param);
-    }
+if (!window.loadSlideShow) {
+    window.loadSlideShow = true
+    init();
+}
 
+function init() {
     if (getQueryParam('subcategory') !== 'paintings') {
         return;
     } else {
         window.changeImage = changeImage;
         fetchImages();
-    }
-
-    function fetchImages() {
-        const categorySelected = getQueryParam('subcategory');
-        fetch('../assets/json/images.json')
-            .then(response => response.json())
-            .then(data => {
-                images = data[categorySelected];; // Get the images based on the selected category
-                showButtons();
-                checkImage();  // Check if the image is selected from the URL
-                loadImages(currentImageIdx, 0);
-            });
-    }
-
-    function checkImage() {
-        const operaQueryParam = getQueryParam('opera');
-        if (operaQueryParam) {
-            const operaTitle = operaQueryParam.replace(/_/g, ' ');
-            const operaIndex = images.findIndex(image => image.title.toLowerCase() === operaTitle.toLowerCase());
-            if (operaIndex !== -1) currentImageIdx = operaIndex;
-        }
-    }
-
-    async function loadImages(index, direction = 0) {
-        if (direction === 0) {
-            const [currBlobUrlInternal, nextBlobUrlInternal, prevBlobUrlInternal] = await Promise.all([
-                loadImageBlob(index),
-                loadImageBlob(index + 1 > images.length - 1 ? 0 : index + 1),
-                loadImageBlob(index === 0 ? images.length - 1 : index - 1)
-            ]);
-            currBlobUrl = currBlobUrlInternal;
-            nextBlobUrl = nextBlobUrlInternal;
-            prevBlobUrl = prevBlobUrlInternal;
-        }
-        if (direction === 1) {
-            URL.revokeObjectURL(prevBlobUrl);
-            prevBlobUrl = currBlobUrl;
-            currBlobUrl = nextBlobUrl;
-            nextBlobUrl = await loadImageBlob(index + 1 > images.length - 1 ? 0 : index + 1);
-        }
-        if (direction === -1) {
-            URL.revokeObjectURL(nextBlobUrl);
-            nextBlobUrl = currBlobUrl;
-            currBlobUrl = prevBlobUrl;
-            prevBlobUrl = await loadImageBlob(index - 1 < 0 ? images.length - 1 : index - 1);
-        }
-        setImageInfo();
-    }
-
-    function setImageInfo() {
-        try {
-            const slide_show = document.getElementById('slide-show');
-            slide_show.style.backgroundImage = `url('${currBlobUrl}')`;
-            const operaNumber = document.getElementsByClassName('opera-number').item(0);
-            operaNumber.innerHTML = `${currentImageIdx + 1} / ${images.length}`;
-            const imageTitle = document.getElementsByClassName('opera-title').item(0);
-            imageTitle.innerHTML = images[currentImageIdx].title;
-            const imageDescription = document.getElementsByClassName('opera-description').item(0);
-            imageDescription.innerHTML = images[currentImageIdx].description;
-            const imageinfo = document.getElementsByClassName('opera-info').item(0);
-            imageinfo.innerHTML = images[currentImageIdx].info;
-            history.pushState({}, null, `?category=${getQueryParam('category')}&subcategory=${getQueryParam('subcategory')}&opera=${images[currentImageIdx].title.replace(/ /g, '_')}`);
-            checkLikeBtn();
-        } catch {
-            console.log('Error while setting image info')
-            $('#main-content').load('../pages/404.html');
-        }
-    }
-
-    async function changeImage(direction) {
-        setCurrentIndex(direction);
-        await loadImages(currentImageIdx, direction);
-    }
-
-
-    function setCurrentIndex(index) {
-        currentImageIdx = (currentImageIdx + index + images.length) % images.length;
-    }
-
-    async function loadImageBlob(index) {
-        const image = images[index];
-        const response = await fetch(image.imgPath);
-        const blob = await response.blob();
-        return URL.createObjectURL(blob);
     }
 
     window.handleArrowKeyPress = function (event) {
@@ -122,7 +39,92 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.addEventListener('keydown', handleArrowKeyPress);
-});
+}
+
+
+function fetchImages() {
+    const categorySelected = getQueryParam('subcategory');
+    fetch('../assets/json/images.json')
+        .then(response => response.json())
+        .then(data => {
+            images = data[categorySelected];; // Get the images based on the selected category
+            showButtons();
+            checkImage();  // Check if the image is selected from the URL
+            loadImages(currentImageIdx, 0);
+        });
+}
+
+function checkImage() {
+    const operaQueryParam = getQueryParam('opera');
+    if (operaQueryParam) {
+        const operaTitle = operaQueryParam.replace(/_/g, ' ');
+        const operaIndex = images.findIndex(image => image.title.toLowerCase() === operaTitle.toLowerCase());
+        if (operaIndex !== -1) currentImageIdx = operaIndex;
+    }
+}
+
+async function loadImages(index, direction = 0) {
+    if (direction === 0) {
+        const [currBlobUrlInternal, nextBlobUrlInternal, prevBlobUrlInternal] = await Promise.all([
+            loadImageBlob(index),
+            loadImageBlob(index + 1 > images.length - 1 ? 0 : index + 1),
+            loadImageBlob(index === 0 ? images.length - 1 : index - 1)
+        ]);
+        currBlobUrl = currBlobUrlInternal;
+        nextBlobUrl = nextBlobUrlInternal;
+        prevBlobUrl = prevBlobUrlInternal;
+    }
+    if (direction === 1) {
+        URL.revokeObjectURL(prevBlobUrl);
+        prevBlobUrl = currBlobUrl;
+        currBlobUrl = nextBlobUrl;
+        nextBlobUrl = await loadImageBlob(index + 1 > images.length - 1 ? 0 : index + 1);
+    }
+    if (direction === -1) {
+        URL.revokeObjectURL(nextBlobUrl);
+        nextBlobUrl = currBlobUrl;
+        currBlobUrl = prevBlobUrl;
+        prevBlobUrl = await loadImageBlob(index - 1 < 0 ? images.length - 1 : index - 1);
+    }
+    setImageInfo();
+}
+
+function setImageInfo() {
+    try {
+        const slide_show = document.getElementById('slide-show');
+        slide_show.style.backgroundImage = `url('${currBlobUrl}')`;
+        const operaNumber = document.getElementsByClassName('opera-number').item(0);
+        operaNumber.innerHTML = `${currentImageIdx + 1} / ${images.length}`;
+        const imageTitle = document.getElementsByClassName('opera-title').item(0);
+        imageTitle.innerHTML = images[currentImageIdx].title;
+        const imageDescription = document.getElementsByClassName('opera-description').item(0);
+        imageDescription.innerHTML = images[currentImageIdx].description;
+        const imageinfo = document.getElementsByClassName('opera-info').item(0);
+        imageinfo.innerHTML = images[currentImageIdx].info;
+        history.pushState({}, null, `?category=${getQueryParam('category')}&subcategory=${getQueryParam('subcategory')}&opera=${images[currentImageIdx].title.replace(/ /g, '_')}`);
+        checkLikeBtn();
+    } catch {
+        console.log('Error while setting image info')
+        $('#main-content').load('../pages/404.html');
+    }
+}
+
+async function changeImage(direction) {
+    setCurrentIndex(direction);
+    await loadImages(currentImageIdx, direction);
+}
+
+
+function setCurrentIndex(index) {
+    currentImageIdx = (currentImageIdx + index + images.length) % images.length;
+}
+
+async function loadImageBlob(index) {
+    const image = images[index];
+    const response = await fetch(image.imgPath);
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+}
 
 // Function to like an image
 window.likeImage = function () {
--- config/navigation.js
+++ config/navigation.js
@@ -1,15 +1,42 @@
 const routes = [
   {
-    href: 'works',
-    text: 'works',
-    subcategories: [
-      { href: 'video_art', text: 'video art' },
-      { href: 'installations', text: 'installations' },
-      { href: 'paintings', text: 'paintings' }
-    ]
+      href: 'works',
+      text: 'works',
+      subcategories: [
+          { 
+              href: 'video_art', 
+              text: 'video art', 
+              file: '../pages/wawa.html'
+          },
+          { 
+              href: 'installations', 
+              text: 'installations', 
+              file: null
+          },
+          { 
+              href: 'paintings',
+              text: 'paintings', 
+              file: '../pages/slideshow.html' }
+      ]
   },
-  { href: 'press', text: 'press' },
-  { href: 'about', text: 'about' },
-  { href: 'exhibitions', text: 'exhibitions' },
-  { href: 'contacts', text: 'contacts' }
+  { 
+      href: 'press', 
+      text: 'press', 
+      file: '../pages/press.html'
+  },
+  { 
+      href: 'about', 
+      text: 'about', 
+      file: '../pages/about.html' 
+  },
+  { 
+      href: 'exhibitions', 
+      text: 'exhibitions', 
+      file: '../pages/exhibitions.html'
+  },
+  { 
+      href: 'contacts', 
+      text: 'contacts', 
+      file: '../pages/contact.html' 
+  }
 ];
