16f662a1503de81c24c807ca211c1681b1bdf1e6
Vincenzo Morisco <vincenzomorisco@hotmail.it>
2024-02-24 19:51:27 +0100

    navigation subcategory removal

--- config/navigation.js
+++ config/navigation.js
@@ -17,36 +17,18 @@ const routes = [
                 href: 'video_art',
                 text: 'video art',
                 file: null
-            }
+            },
         ]
     },
     {
         href: 'about',
         text: 'about / exhibitions',
-        file: '../pages/about.html',
-
+        file: '../pages/about.html'
     },
     {
         href: 'press',
         text: 'press',
-        file: '../pages/press.html',
-        subcategories: [
-            {
-                href: 'paintings',
-                text: 'paintings',
-                file: '../pages/slideshow.html'
-            },
-            {
-                href: 'installations',
-                text: 'installations',
-                file: null
-            },
-            {
-                href: 'video_art',
-                text: 'video art',
-                file: null
-            }
-        ]
+        file: '../pages/press.html'
     },
     {
         href: 'contacts',
